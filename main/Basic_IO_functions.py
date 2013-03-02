737ed47ef1311f7b5eab6992e83fcf6f7b712717
Andrew Cox <andrewelliotcox@gmail.com>
2013-03-02 10:34:56 -0600

    Created a Basic IO function to begin separating out the IO functions of this effort

--- main/Basic_IO_functions.py
+++ main/Basic_IO_functions.py
@@ -8,7 +8,7 @@ import numpy as np
 
 #create the data array from any csv file
 def read_csv_into_array (csv_file):
-    file_reader = csv.reader(open("../"+csv_file))#append the ../ to find the file in the current direcctory
+    file_reader = csv.reader(open("../"+csv_file))#append the ../ to find the file in the current directory
     file_reader.next()#skips the header row
     file_list = []#defines a list that we'll read the file into
     for row in file_reader:
@@ -16,6 +16,14 @@ def read_csv_into_array (csv_file):
     file_array = np.array(file_list)
     return file_array
 
+#output a .csv file from either a list or an array
+def convert_into_csv (array_to_file, output_name):
+    array_writer = csv.writer(open("../main/"+output_name+".csv", "wb"))
+    for row in array_to_file:
+        array_writer.writerow(row)
+    return
+
+
 #train_array = read_csv_into_array("train.csv")
 #print train_array[0]
-
+#convert_into_csv(train_array, "test_csv_file")
