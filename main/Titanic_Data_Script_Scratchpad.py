d1b377ddd5db87aa18ae3f7da891dd9767329206
Andrew Cox <andrewelliotcox@gmail.com>
2013-03-06 07:53:06 -0800

    Created a python "scratchpad" file that is trying (trying) to create a 2 column array with survival values line up with gender.  Instead I created a 2 ROW array that does that and I can't figure out how to switch it.

---
+++ main/Titanic_Data_Script_Scratchpad.py
@@ -0,0 +1,22 @@
+__author__ = 'Andrew'
+
+import numpy as np
+from random import random, randrange
+from pylab import *
+import matplotlib
+import numpy as np
+import Basic_IO_functions as io
+
+#get the csv file and convert into an array
+train_array = io.read_csv_into_array("train.csv")
+
+#do a linear regression on the array against the first column
+#Create a 2d array of just survive and some other element
+survive_array = np.array([train_array[:,0]])
+gender_array = np.array([train_array[:,3]])
+
+survive_gender_array = np.concatenate([survive_array, gender_array])
+survive_gender_array.transpose()
+
+#print out that result in a table
+#io.convert_into_csv(survive_gender_array,"array concat")
