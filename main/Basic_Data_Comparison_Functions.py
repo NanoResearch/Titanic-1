002ed8616254a6619bd33556f2ef99b58a1c8cf6
Andrew Cox <andrewelliotcox@gmail.com>
2013-05-31 22:25:57 -0700

    Wrote a function called "find high-value pairs" which essentially finds the pairs that are most correlated with an outcome as compared to the opposite outcome (e.g male is correlated with outcome 0 4.28 times more than 1).  The function does not find correlations between the predictive pairs.

--- main/Basic_Data_Comparison_Functions.py
+++ main/Basic_Data_Comparison_Functions.py
@@ -22,6 +22,7 @@ def compare_columns_in_array(array_to_be_compared, anchor_column_index=0, anchor
     column_stats_array = ([])
     pair_count_array = ([])
     all_counts_array = ([])
+    pair_count_dictionary = {}
 
     #extract comparison values from anchor and comparison arrays
     anchor_column_values_list = extract_comparison_values_from_array(anchor_column_array,0,0,"clco",0)
@@ -36,29 +37,32 @@ def compare_columns_in_array(array_to_be_compared, anchor_column_index=0, anchor
         #iterate through the comparison value list and compare the anchor value to each value in the comparison value list
         #reset ii for next comparison value
         ii = 0
-        while ii < (comparison_column_values_list.__len__()):
+        while ii < (comparison_column_values_list.__len__()): #find each unique comparison value in the column.  the iii loop then counts how many pairs there are with an anchor value
             compared_to_column_value = comparison_column_values_list[ii][0]
 
             iii = 0
             pair_count = 0
-            while iii < (number_of_rows-1):
+            while iii < (number_of_rows-1): #iterate through each row in the anchor and compared column to find pairs
                 if ( (anchor_column_array[iii]== anchor_column_value) and (compared_column_array[iii]==compared_to_column_value)):
                     #need to store the count only
                     pair_count = pair_count+1
                 iii = iii + 1
                 #need to store the pair of anchor and comparison and their count
-            pair_count_array.append([anchor_column_value, compared_to_column_value, pair_count])
-            ii = ii + 1
+            #consider using a dictionary to pull out counts by unique pair key
+            if pair_count >=30:
+                pair_count_array.append(["Clm"+compared_to_column_index.__str__(), anchor_column_value, compared_to_column_value, pair_count])
+                #pair_count_dictionary["Clm"+compared_to_column_index.__str__()+"Acr:"+anchor_column_value + "/Cpr:" + compared_to_column_value]= [anchor_column_value, compared_to_column_value, pair_count]
 
-            #possible to limit this loop by filtering out values that have less than 30 occurrences (< than what a t test requires)
-        #After each anchor value pairing, extract the pair_count_array lists, put their real values into an array, and erase pair_count_array
-        all_counts_array.append(["column "+compared_to_column_index.__str__(),pair_count_array])
-        pair_count_array = ([])
+            ii = ii + 1
+            #end while ii loop
 
         #repeat for next value on the anchor list
         i = i + 1 # iterate for the nested while loop
 
-    return all_counts_array
+    #print pair_count_dictionary
+    return pair_count_array
+    #return pair_count_dictionary
+    #return all_counts_array
 
 def extract_comparison_values_from_array(array_to_be_compared, column_index=0, row_index =0, output_type="clco", minimum_number_of_elements = 30):#need to document function
 
@@ -146,19 +150,100 @@ def compare_all_columns_in_array(array_to_be_compared, anchor_column_index=0, an
     number_of_rows = array_shape[0]
 
     all_columns_compared_array = ([])
+    #temp_array = ([])
     i = 0
 
     #compare_column_index_list
-    #limit to values who have counts over 30
+    #limit to values who have counts over 30 possibly.  proxy for statistical significance
+    #need to clean up outputs to not be a bunch of lists
     while i < (number_of_columns-1):
         if i != anchor_column_index:
+            #temp_array = compare_columns_in_array(array_to_be_compared, anchor_column_index, anchor_column_value, i)
             all_columns_compared_array.append(compare_columns_in_array(array_to_be_compared, anchor_column_index, anchor_column_value, i))
+            #all_columns_compared_array.append("")
         i = i+1
-
+    #print "The shape of all_columns_compared_array is... "+np.shape(all_columns_compared_array).__str__()
     return all_columns_compared_array
+
+def find_high_value_comparison_pairs (array_to_be_compared):#use the dictionary or array to find values that are highly correlated with the anchor values (1 or 0)
+    #find entries where the column is the same, the anchor value differs, but the comparison value is the same
+    #EAch list is composed of [ClmX, anchor_column_value, compared_to_column_value, pair_count].  One list per pair match,
+    original_comparison_pairs_array = compare_all_columns_in_array(array_to_be_compared)
+    row_list = []
+    zero_list = []
+    one_list = []
+    rules_array = ([])
+    #rules_array_ordered = ([])
+    rule_prediction_factor = 0 #equals prediction_ratio (accuracy) * comparitor_count (reliability)
+    prediction_ratio = 0 #numerator is the number of outcome-comparitor pairs for outcome that is predicted more over the number of outcome-comparitor pair that is predicted less.  The greater the difference, the more the comparitor predicts the numerator outcome
+    comparitor_count = 0 #the count of rows that have the comparitor in it regardless of whether it lines up against 1 or 0
+    comparitor_percentage = 0 #the comparitor count over the overall sample size
+    comparitor_outcome_percentage = 0
+    overall_sample_size = 892 #total number of records
+
+    i=0
+    ii=0
+    iii=0
+    iiii=0
+    while i < original_comparison_pairs_array.__len__():
+        row_list = original_comparison_pairs_array[i]
+
+        if row_list.__len__() > 0:
+            ii = 0
+            while ii < row_list.__len__():
+
+                    if (row_list[ii][1] == '0'):
+                        zero_list = row_list[ii]
+
+                        iii = 0
+                        while iii < row_list.__len__():
+                            #!!!!zero_list is probably somewhat messed up.  It might need an iii index!!!  SEEE PRINT OUTS ABOVE
+                            #print " in  iii while " + iii.__str__()
+                            if row_list[iii][1] == "1" and row_list[iii][2] == zero_list[2]: # find the comparison value
+                                if row_list[iii][3] > zero_list[3]:
+                                    rule_prediction_factor = ((float(row_list[iii][3])/float(zero_list[3]))*(row_list[iii][3]+zero_list[3]))
+                                    rules_array.append([row_list[iii][1], row_list[iii][2], row_list[iii][2]+" in column "+row_list[iii][0] +" has a prediction factor of "+rule_prediction_factor.__str__() + " for outcome flag"+ row_list[iii][1], rule_prediction_factor, "prediction ratio is "+(float(row_list[iii][3])/float(zero_list[3])).__str__()])
+                                else:
+                                    rule_prediction_factor = (float(zero_list[3])/float(row_list[iii][3])) * (zero_list[3]+row_list[iii][3])
+                                    rules_array.append([zero_list[1],zero_list[2], zero_list[2].__str__()+" in column "+zero_list[0] + " has a prediction factor of "+rule_prediction_factor.__str__()+" for outcome flag "+zero_list[1], rule_prediction_factor, "prediction ratio is "+(float(zero_list[3])/float(row_list[iii][3])).__str__()])
+                                #write out that it's predictive of the correct anchor + the higher count over the lower count
+
+                            iii = iii + 1
+                    ii = ii + 1
+
+        i = i+1
+        #take the one that has the higher count and put its count as numerator and the lower count as denominator
+        #the result gives a ratio or score which shows how differently each comparison value predicts the anchor
+        #using a function flag, choose only pairs where the ratio is above some number (e.g. above 1.5)
+        #append these into an array and return it
+        #this should show which values have high leverage.
+        #later this could be done across an anchor and multiple comparison values.
+    rules_array_list_a = []
+    rules_array_list_b = []
+    swapped = True
+    #print rules_array
+    while (swapped):
+        swapped = False
+        while iiii < (rules_array.__len__()-1):
+            rules_array_list_a = rules_array[iiii]
+            rules_array_list_b = rules_array[iiii+1]
+            if rules_array_list_b[3] > rules_array_list_a[3]:
+                print "Switch took place "+iiii.__str__()
+                rules_array[iiii]= rules_array_list_b
+                rules_array[iiii+1] = rules_array_list_a
+                swapped = True
+            print swapped
+            iiii = iiii + 1
+        if swapped == True:
+            iiii = 0
+    print "rules_array"
+    print rules_array
+    return rules_array
+
 #scripting lines for test purposes
-train_array = io.read_csv_into_array("train.csv")
-io.convert_into_csv(compare_all_columns_in_array(train_array, 0), "comparison values")
+train_array = io.read_csv_into_array("train.B.csv")
+io.convert_into_csv(find_high_value_comparison_pairs(train_array), "prioritized rules")
+#io.convert_into_csv(compare_all_columns_in_array(train_array, 0), "comparison values")
 
 
 # print extract_comparison_values_from_array(train_array[:,1])#how do I do default values?
